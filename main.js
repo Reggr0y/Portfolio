f9535906dee6bc671ab9e1ffa383cf3e15e9a3aa
Reggr0y <gregory.wd@pm.me>
2022-06-26 22:45:39 +0200

    Update the welcome message and include hello back

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
     let myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome, ' + myName;
+    myHeading.textContent = 'I am Greg, welcome ' + myName;
   }
   
-
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello back, ' + storedName;
+  }
+  
 

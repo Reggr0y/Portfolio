bc2e80f8dbf4e1cedba4e77b7c2057ecf7307029
Reggr0y <gregory.wd@pm.me>
2022-06-26 23:30:18 +0200

    Add a new rule for HTML img but there is an issue with the size
    Despite having a ".style.width = "300px";" following function on line 27 the img is still displaying in the wrong size

--- main.js
+++ main.js
@@ -19,4 +19,14 @@ let myName = prompt('Please enter your name.');
     myHeading.textContent = 'Hello back, ' + storedName;
   }
   
+  let myHTML = document.querySelector('#html');
 
+  myHTML.onclick = function() {
+      let mySrc = myHTML.getAttribute('src');
+      if(mySrc === 'https://seeklogo.com/images/H/html5-with-wordmark-black-white-logo-A6222B77EC-seeklogo.com.png') {
+        myHTML.setAttribute('src','https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png').style.width = "300px";
+      } else {
+        myHTML.setAttribute('src','https://seeklogo.com/images/H/html5-with-wordmark-black-white-logo-A6222B77EC-seeklogo.com.png');
+      }
+  }
+  
